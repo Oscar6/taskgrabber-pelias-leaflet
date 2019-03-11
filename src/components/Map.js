01b4cd9917bafadcae5cd882c610aae42fcebd5b
Oscar6 <oscar.m1024@gmail.com>
2019-03-10 21:48:31 -0500

    markers added to map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,33 +1,50 @@
 import React from "react";
-import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
-import styled from 'styled-components';
+import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet'
+import '../Styles/App.css';
 
+  
+  export const storeIcon = new L.Icon({
+    iconUrl: require('../assets/store.png'),
+    iconRetinaUrl: require('../assets/store.png'),
+    iconAnchor: [5, 55],
+    popupAnchor: [10, -44],
+    iconSize: [25, 30],
+    shadowSize: [68, 95],
+    shadowAnchor: [20, 92],
+  })
 
-const Wrapper = styled.div `
-    width: ${props => props.width};
-    height: ${props => props.height};
-    border-style: ${props => props.borderStyle};
-    border-width: ${props => props.borderWidth};
-    border-radius: ${props => props.borderRadius};
-`;
-
-export default class Map extends React.Component {
-    
-    componentDidMount(){
-        this.map = L.map('map', {
-            center: [29.7604, -95.3698],
-            zoom: 12,
-            zoomControl: false
-        });
-        L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
-            detectRetina: true,
-            maxZoom: 20,
-            maxNative: 17,
-        }).addTo(this.map);
-    }
+class Map extends React.Component {
 
     render() {
-        return <Wrapper width="550px" height="550px" borderWidth="2px" borderStyle="solid" borderRadius="20px" id="map" />
+      return (
+        <LeafletMap
+          center={[29.7604, -95.3698]}
+          zoom={11}
+          maxZoom={18}
+          attributionControl={true}
+          zoomControl={false}
+          doubleClickZoom={true}
+          scrollWheelZoom={true}
+          dragging={true}
+          animate={true}
+          easeLinearity={0.35}>
+          <TileLayer
+            url='https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}'
+          />
+          <Marker position={[29.7316085, -95.4649623]} icon={storeIcon}>
+            <Popup>
+              Popup for any custom information.
+            </Popup>
+          </Marker>
+          <Marker position={[29.754009, -95.527836]} icon={storeIcon}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+        </LeafletMap>
+      );
     }
-}
+  }
+  
+  export default Map
